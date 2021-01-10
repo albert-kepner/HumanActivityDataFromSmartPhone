857a0824a8f6c7c428ec52ab3f9e1f2fed57b23a
Al Kepner <albert.kepner@outlook.com>
2021-01-10 10:11:53 -0500

    downloaded zip file

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,22 @@
 ## Source File: run_analysis.R
+## Getting and Cleaning Data Course Week 4 Project -- by Al Kepner
 
+## This is the data from the course website.
 ## Download data from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
+## The data set is originally from here: (UCI Machine Learning Repository)
+## http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+## Download the data from the Cousera course website:
 dataPath <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 destinationPath <- "data/projectFiles.zip"
 
 if(!file.exists("data")) {
-  create.dir("data")
+  dir.create("data")
 }
 
-download.file(url=dataPath,mode="auto", method="wb", destfile=destinationPath)
+download.file(url=dataPath,mode="wb", method="auto", destfile=destinationPath)
+
+zipFile <- destinationPath
+outDir<-"data"
+unzip(zipFile,exdir=outDir)
